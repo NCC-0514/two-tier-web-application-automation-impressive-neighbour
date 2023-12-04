45cd5fc066dbcc6c59beab029b3d1981c88dc29e
MasihZamani <mzamani11@myseneca.ca>
2023-12-04 12:49:56 -0500

    adding alb.tf

---
+++ ASG ALB/alb.tf
@@ -0,0 +1,43 @@
+# ALB Configuration
+resource "aws_lb" "example" {
+  name               = "${local.name_prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public_subnet[*].id
+
+  enable_deletion_protection = false
+
+  tags = local.default_tags
+}
+
+# Target Group for ALB
+resource "aws_lb_target_group" "example" {
+  name     = "${local.name_prefix}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    interval            = 30
+    path                = "/"
+    protocol            = "HTTP"
+  }
+
+  tags = local.default_tags
+}
+
+# Listener for ALB
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
