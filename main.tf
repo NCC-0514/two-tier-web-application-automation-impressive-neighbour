fae4471029d17166a8dac24e37706d66fffb5005
EC2 Default User <ec2-user@ip-172-31-92-32.ec2.internal>
2023-12-04 00:12:58 +0000

    new changes

--- main.tf
+++ main.tf
@@ -83,3 +83,7 @@ resource "aws_route_table_association" "public_route_table_association" {
   route_table_id = aws_route_table.public_route_table.id
   subnet_id      = aws_subnet.public_subnet[count.index].id
 }
+
+
+
+# my changes
