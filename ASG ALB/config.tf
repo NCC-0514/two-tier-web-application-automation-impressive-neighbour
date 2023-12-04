13a9907fc7f04db83ba5cc7778fa235cf418269d
MasihZamani <mzamani11@myseneca.ca>
2023-12-04 12:57:02 -0500

    adding main.tf config.tf, no changes

---
+++ ASG ALB/config.tf
@@ -0,0 +1,9 @@
+# config.tf
+
+terraform {
+  backend "s3" {
+    bucket = "project-komal"      // Bucket where to SAVE Terraform State
+    key    = "staging-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
+    region = "us-east-1"                     // Region where bucket is created
+  }
+}
