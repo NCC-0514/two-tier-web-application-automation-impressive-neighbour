2a73f1482cdf8d022d445655d1d5e184fbab2ab7
komals-1494 <komals.1494@gmail.com>
2023-12-10 07:16:24 +0000

    final testing

--- terraform/network/config.tf
+++ terraform/network/config.tf
@@ -5,3 +5,17 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_dynamodb_table" "s3_lock_table" {
+  name           = "S3LockTable"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "BucketName"
+    type = "S"
+  }
+
+  hash_key = "BucketName"
+}
